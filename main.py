d1f8739ea41152085a96e7c0a52780bbfb7e65d2
Claudio Filipe <145695230+cfdalalbaa@users.noreply.github.com>
2026-06-11 19:49:13 -0300

    Add print statement for quitting event

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ window = pg.display.set_mode(size=(600, 480))
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
+            print('Quitting')
             pg.quit()
             quit()
