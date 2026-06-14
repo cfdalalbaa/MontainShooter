9ea60d6351f133f8640874ff9965d21dc065192c
Claudio Filipe <claudio.filipe.11@hotmail.com>
2026-06-14 17:02:44 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file.

---
+++ asset/Const.py
@@ -0,0 +1,9 @@
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2 - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+WIND_WIDTH = 576
+WIND_HEIGHT = 324
--- code/game.py
+++ code/game.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
 import pygame as pg
 
+from asset.Const import WIND_WIDTH, WIND_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pg.init()
-        self.window = pg.display.set_mode(size=(600, 480))
+        self.window = pg.display.set_mode(size=(WIND_WIDTH, WIND_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
-            # for event in pg.event.get():
-            #     if event.type == pg.QUIT:
-            #         pg.quit()
-            #         quit()
